a1ac326ef3ac7f3771b4eadd3918f81f9833a7ee
PierreLan <pierre.langrenay@gmail.com>
2015-12-26 12:28:57 +0800

    Solution to problem 002

---
+++ 002/002/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long limit = 4000000;
+            long prev = 1;
+            long next = 2;
+            long sum = 0;
+
+            while (prev < limit)
+            {
+                if (prev % 2 == 0)
+                {
+                    sum += prev;
+                }
+                long tmp = next;
+                next = prev + next;
+                prev = tmp;
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
